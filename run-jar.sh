730eca4895fd78c9076d90cf5c801d2fc1b3cda5
LiJohnson <LiJohnson@users.noreply.github.com>
2019-03-08 23:28:39 +0800

    Update run-jar.sh

--- run-jar.sh
+++ run-jar.sh
@@ -2,7 +2,7 @@
 # by lcs
 # create 2018-03-29
 #
-# update 2019-01-31	by lcs
+# update 2019-01-31 by lcs
 
 JAR_FILE=`ls $1`
 JAR_FILE_NAME=`basename $JAR_FILE`
@@ -15,18 +15,18 @@ function kill_pid() {
     PID_EXIST=`ps -f -p ${PID} | grep java`
     echo "killing $PID ==> $PID_EXIST"
     kill ${PID}
-	while [[ ${COUNT} -lt 10 ]]; do
-	    PID_EXIST=`ps -f -p ${PID} | grep java`
-	    if [[ -n "$PID_EXIST" ]]; then
-	        echo ""
+    while [[ ${COUNT} -lt 10 ]]; do
+        PID_EXIST=`ps -f -p ${PID} | grep java`
+        if [[ -n "$PID_EXIST" ]]; then
+            echo ""
             return
         fi
         echo -e ".\c"
         sleep 1
         COUNT=$((COUNT+1))
-	done
-	echo "\n kill -9 $PID"
-	kill -9 ${PID}
+    done
+    echo "\n kill -9 $PID"
+    kill -9 ${PID}
 }
 
 PIDS=`ps -ef | grep java | grep -v grep | grep "$JAR_FILE" |awk '{print $2}'`
@@ -35,9 +35,9 @@ if [ -n "$PIDS" ]; then
     echo "The $JAR_FILE is running !"
     echo "PID: $PIDS"
     for PID in $PIDS ; do
-    	kill_pid $PID
-	done
-	sleep 1
+        kill_pid $PID
+    done
+    sleep 1
 fi
 
 echo "LOG_FILE : $LOG_FILE"
